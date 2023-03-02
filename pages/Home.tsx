c39965cc0fd8a4fb191e3cfa337b9d7d19a9c631
harryFBloch <harryfbloch@gmail.com>
2023-03-02 16:51:40 -0500

    work on inputs

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,12 +1,15 @@
 import { ReactElement, useEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, View, Animated } from 'react-native';
-import { Button, Card, Image } from '@rneui/themed';
+import { Button, Card, Image, Input } from '@rneui/themed';
 import { Segment } from '../components/Segment';
 import { TankShapes } from '../store/types';
 
 export const Home = (): ReactElement => {
 
   const [tabIndex, setTabIndex] = useState(0);
+  const [width, setWidth] = useState("");
+  const [height, setHeight] = useState("");
+  const [depth, setDepth] = useState("");
   const selectedShape = Object.values(TankShapes)[tabIndex];
   const imageAnimation = useRef(new Animated.Value(1)).current
 
@@ -38,7 +41,30 @@ export const Home = (): ReactElement => {
       }).start(() => {
         setTabIndex(index);
       })
-    
+  }
+
+
+  const renderRectangleInputs = (): ReactElement => {
+    return (
+      <View>
+        <Input value={String(height)} placeholder="Height" keyboardType="number-pad" onChangeText={(e) => {setHeight(e)}}/>
+        <Input value={String(width)} placeholder="Width" keyboardType="number-pad" onChangeText={(e) => setWidth(e)}/>
+        <Input value={String(depth)} placeholder="Depth" keyboardType="number-pad" onChangeText={(e) => setDepth(e)}/>
+      </View>
+    )
+  }
+
+  const renderInputs = ():ReactElement => {
+    switch (tabIndex){
+      case 0:
+        return <></>
+      case 1:
+        return renderRectangleInputs();
+      case 2:
+        return <></>
+      default:
+        return <></>
+    }
   }
 
 
@@ -48,6 +74,7 @@ export const Home = (): ReactElement => {
       <Card.Divider/>
       <Segment selectedIndex={tabIndex} callback={handleSelectShapeCallback}/>
       {renderTankShape()}
+      {renderInputs()}
     </Card>
   
   )
